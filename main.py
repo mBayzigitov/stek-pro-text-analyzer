cce9d922e82990c3c9e4f122234ef2d1310e6a73
Murat <bayzigitov.murat@gmail.com>
2024-09-25 16:12:01 +0300

    upd: save lc/wp implemented

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import sys
-import re
+import sys, re, glob
 from collections import Counter
 
 TOP_FREQ_WORDS = 10
 INCORRECT_COMMAND_MSG = "\tНекорректная команда, введите \"help\", чтобы посмотреть список команд"
+SAMPLE_NAME = "output-"
+SAMPLE_EXT = ".txt"
 
 def read_file(filename):
-    """Чтение файла и возврат его содержимого как строки."""
+    """Чтение файла и возврат его содержимого как строки"""
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             return file.read()
@@ -19,7 +20,7 @@ def count_lines(text):
     return len(lines), lines
 
 def count_words(text):
-    words = re.findall(r'\b\w+\b', text)
+    words = re.findall(r'\b[\w-]+\b', text)
     return len(words), [w.lower() for w in words]
 
 def count_characters(text):
@@ -40,6 +41,7 @@ def find_longest_word(words):
     return None
 
 def find_lines_word_occurs_in(target, lines):
+    """Найти индексы строк, в которых есть слово target"""
     res = []
     for i in range(len(lines)):
         for word in lines[i]:
@@ -56,11 +58,12 @@ def print_refs():
     print()
 
 def print_lines_word_occurs_in(target, ws_by_lines, cache, lines):
-    res_lines = {}
+    """Вывести на экран строки, в которых есть слово target"""
+    res_lines = [] # итоговый список индексов строк
     if target in cache:
-        res_lines = cache[target]
+        res_lines = cache[target] # если это слово часто употребляется - взять из "кэша"
     else:
-        res_lines = find_lines_word_occurs_in(target, ws_by_lines)
+        res_lines = find_lines_word_occurs_in(target, ws_by_lines) # если нет - выполнить поиск
     # Вывести строки на экран
     if len(res_lines) == 0:
         print("\tДанное слово не найдено в тексте")
@@ -70,6 +73,7 @@ def print_lines_word_occurs_in(target, ws_by_lines, cache, lines):
         print(f"\t{lines[res]}")
 
 def print_word_by_index(idx, borders, words_by_lines):
+    """Вывести на экран слово по номеру idx"""
     l = 0
     h = len(borders) - 1
     mid = 0
@@ -85,11 +89,24 @@ def print_word_by_index(idx, borders, words_by_lines):
             print(words_by_lines[mid][idx - borders[mid][0]])
             return
 
+def count_existing_files():
+    return len(glob.glob(SAMPLE_NAME + "*" + SAMPLE_EXT))
+
+def write_sample_file_content(text, ftype):
+    n = count_existing_files()  # вычислить номер нового файла
+    try:
+        with open(SAMPLE_NAME + str(n) + SAMPLE_EXT, "x", encoding='utf-8') as file:
+            file.write(text.lower())
+    except IOError:
+        print("\t! Возникла ошибка, файл не записан")
+        return
+    print(f"\tТекст {ftype} успешно записан в файл {SAMPLE_NAME + str(n) + SAMPLE_EXT}")
+
 def main():
     if len(sys.argv) != 2:
         print("Использование: python main.py [filename]")
         sys.exit(1)
-    
+
     filename = sys.argv[1]
     text = read_file(filename)
 
@@ -113,14 +130,14 @@ def main():
     print(f"Количество строк: {num_lines}")
     print(f"Количество слов: {num_words}")
     print(f"Количество символов: {num_chars}")
-    
+
     if repeated_words:
         print("Слова, которые повторяются более одного раза:")
         for word, count in repeated_words.items():
             print(f"  {word}: {count} раз(а)")
     else:
         print("Нет слов, которые повторяются более одного раза")
-    
+
     if longest_word:
         print(f"Самое длинное слово: {longest_word}")
     else:
@@ -148,7 +165,7 @@ def main():
     input_line = ""
     while input_line != "exit":
         input_line = input(">> ")
-        spl = input_line.split()
+        spl = input_line.rstrip().split()
         command = spl[0]
 
         if command == "help":
@@ -158,16 +175,25 @@ def main():
             print(INCORRECT_COMMAND_MSG)
             continue
 
+        subcommand = spl[1]
+
         if command == "fword":
-            print_lines_word_occurs_in(spl[1].lower(), words_by_lines, freq_words_cache, lines)
+            print_lines_word_occurs_in(subcommand.lower(), words_by_lines, freq_words_cache, lines)
         elif command == "fnum":
             try:
-                number = int(spl[1])
+                number = int(subcommand)
                 if number <= 0 or number > num_words:
                     print(f"Некорректный номер слова, всего слов в тексте: {num_words}")
                     continue
                 print_word_by_index(number, line_words_minmax_number, words_by_lines)
             except ValueError:
+                print(INCORRECT_COMMAND_MSG)
+        elif command == "save":
+            if subcommand == "lc":
+                write_sample_file_content(text.lower(), "в нижнем регистре")
+            elif subcommand == "wp":
+                write_sample_file_content(re.sub('[^\w\s]+', '', text), "без знаков препинания")
+            else:
                 print(INCORRECT_COMMAND_MSG)
 
 if __name__ == "__main__":
